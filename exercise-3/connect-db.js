cd128a57bab0afe78cc7ada4534f0ed6f9d346ad
Pedro Tashima <pedrotashima42@gmail.com>
2021-12-02 21:23:39 -0300

    add exercise 3

---
+++ exercise-3/connect-db.js
@@ -0,0 +1,47 @@
+const { MongoClient } = require('mongodb');
+const mysql = require("mysql2")
+
+main()
+
+async function main() {
+  // MONGODB
+  const client = await connectDb("mongodb://localhost", 27017, true)
+  const db = client.db("test");
+  const collection = db.collection("test");
+  const result = await collection.find({}).toArray();
+  console.log(result)
+  // MYSQL
+  /*
+  const client = await connectDb("127.0.0.1", 3306, false)
+  client.query(
+    'SELECT * FROM plano;',
+    (err, results, fields) => console.log(results)
+  )
+  */
+}
+
+async function connectDb(ip, port, isMongoConnection) {
+  if(isMongoConnection) {
+    return await connectMongoDb(ip, port)
+  }     
+  return await connectMySql(ip, port)
+}
+
+async function connectMongoDb(ip, port = 27017) {
+  const url = `${ip}:${port}` 
+  const client = new MongoClient(url)
+  await client.connect()
+  return client
+}
+
+async function connectMySql(ip, port) {
+  const client = mysql.createConnection({
+    host: ip,
+    port,
+    user: "root",
+    password: "root",
+   database : 'netbaseofdata'
+  })
+  await client.connect()
+  return client
+}
